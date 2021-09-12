e499269923d128aaf13123a0299c17bf7e950b44
santhossunthar <santhoshsunthar@gmail.com>
2021-09-13 01:48:50 +0530

    Added startup function of program UI

---
+++ ende.py
@@ -0,0 +1,12 @@
+import sys
+from PyQt5 import QtGui, QtWidgets
+from main_window import MainWindow
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    myapp = MainWindow()
+    myapp.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
