e918f5808c7ae1f87e8a60f65788f7df6594ab79
santhossunthar <santhoshsunthar@gmail.com>
2021-09-14 02:00:15 +0530

    Added DataInsertionWindow class

---
+++ data_insertion_window.py
@@ -0,0 +1,12 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class DataInsertionWindow(QtWidgets.QDialog):
+    def __init__(self, parent = None):
+        super(DataInsertionWindow, self).__init__()
+
+        
+
+
+
+    
+
